27b485b7dd3af17798dcf980a0e32365b27213b2
tifuseini <tifuseini@st.ug.edu.gh>
2023-01-13 12:16:25 +0000

    order service kafka producer config

---
+++ order-service/src/main/java/com/sosa/orderservice/config/KafkaConfig.java
@@ -0,0 +1,4 @@
+package com.sosa.orderservice.config;
+
+public class KafkaConfig {
+}
--- order-service/src/main/java/com/sosa/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/sosa/orderservice/controller/OrderController.java
@@ -1,11 +1,13 @@
 package com.sosa.orderservice.controller;
 
 import com.sosa.orderservice.dto.OrderRequest;
+import com.sosa.orderservice.event.OrderPlaced;
 import com.sosa.orderservice.models.Order;
 import com.sosa.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,9 +16,12 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class OrderController {
 
+    private final KafkaTemplate<String, OrderPlaced> kafkaTemplate;
+
     @GetMapping
     public String getOrder() {
         log.info("Order retrieved");
+        kafkaTemplate.send("order-test", new OrderPlaced("1", "1", 1, "1"));
         return "Order retrieved";
     }
 
