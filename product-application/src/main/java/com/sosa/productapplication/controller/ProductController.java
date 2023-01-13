21da01bdc5f6566268afc04f0608f48683187d96
tifuseini <tifuseini@st.ug.edu.gh>
2023-01-13 10:16:36 +0000

    removed security from services

--- product-application/src/main/java/com/sosa/productapplication/controller/ProductController.java
+++ product-application/src/main/java/com/sosa/productapplication/controller/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/product2")
+@RequestMapping("/api/v1/product-application")
 @Slf4j
 public class ProductController {
 
